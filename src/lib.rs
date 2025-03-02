6de791b8bbbee7b9fcb32e8af42017999f3be1a1
clearlysid <siddharth99c@gmail.com>
2025-03-03 01:32:08 +0530

    feat: adds structs as per specs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// Decodes `EncodedAudioChunk` objects.
+pub struct AudioDecoder {}
+
+/// Decodes `EncodedVideoChunk` objects.
+pub struct VideoDecoder {}
+
+/// Encodes `AudioData` objects.
+pub struct AudioEncoder {}
+
+/// Encodes `VideoFrame` objects.
+pub struct VideoEncoder {}
+
+/// Represents codec-specific encoded audio bytes.
+pub struct EncodedAudioChunk {}
+
+/// Represents codec-specific encoded video bytes.
+pub struct EncodedVideoChunk {}
+
+/// Represents unencoded audio data.
+pub struct AudioData {
+    duration: f64,
+    format: String,
+    number_of_channels: u32,
+    number_of_frames: u32,
+    sample_rate: u32,
+    timestamp: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Represents a frame of unencoded video data.
+pub struct VideoFrame {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Represents the color space of a video frame.
+pub struct VideoColourSpace {}
+
+/// Unpacks and decodes image data, giving access to the sequence of frames in an animated image.
+pub struct ImageDecoder {}
+
+/// Represents an individual image track.
+pub struct ImageTrack {}
+
+/// Represents the list of tracks available in the image.
+pub struct ImageTrackList {}
